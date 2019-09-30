43e3d8e38f57b105ea49b0ec9d5b9fb8223d8309
Ubuntu <ubuntu@ip-172-26-1-152.ec2.internal>
2019-09-30 06:50:10 +0000

    mejor ordenadas las columnas

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import pandas as pd
 
 df = pd.read_parquet('DATA/derechos_de_agua.pq')
 df = df.sort_values('Caudal_Promedio', ascending=False)
+dfc = list(df.columns)
+dfc = dfc[:7]+['Caudal_Promedio']+dfc[7:-1]    # para la visualizacion inmediata
+df = df[dfc]
 
 app = dash.Dash(__name__)
 
